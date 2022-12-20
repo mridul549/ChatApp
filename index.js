4839c5756c779b3f3f4f7934a14d3b2edfd2131e
mridul549 <mridulverma478@gmail.com>
2022-12-21 00:03:18 +0530

    Version 2.0.3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const date    = require(__dirname + "/date.js");
 const app=express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')));
 
 const day=date.getDate();
 const time=date.getTime();
